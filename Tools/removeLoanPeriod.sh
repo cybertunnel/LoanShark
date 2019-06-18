5cb0bc94c635e45f01675594c563ab0d3dfbd5f0
Tyler Morgan <tylerwazhere@aim.com>
2019-06-18 15:12:36 -0400

    Fixed few bugs

--- Tools/removeLoanPeriod.sh
+++ Tools/removeLoanPeriod.sh
@@ -5,9 +5,14 @@
 # This script will reset the loan period that has been set on the machine.
 #####################
 
-$find="/usr/bin/find"
+find="/usr/bin/find"
+killall="/usr/bin/killall"
 
-$find /Users/ -name "com.github.cybertunnel.LoanShark.plist" -delete
+app_name="LoanShark"
+
+$find /Users/ -name "com.github.cybertunnel.LoanShark.plist" -delete 2> /dev/null
+
+$killall "${app_name}"
 
 exit 0
 
