33eacab016209798901a3b4d5ed0ff7ea732f19c
Tyler Morgan <tylermorgan210@gmail.com>
2019-12-24 10:24:23 -0500

    Forgot to add outlets

--- LoanShark/Views/LoanerConfigurationViewController.swift
+++ LoanShark/Views/LoanerConfigurationViewController.swift
@@ -10,58 +10,6 @@ import Cocoa
 
 class LoanerConfigurationViewController: NSViewController {
     
-    //  MARK: Cocoa Binding Resources
-    @objc var userFirst: String? {
-        didSet {
-            self.checkRequirements()
-        }
-    }
-    @objc var userLast: String? {
-        didSet {
-            self.checkRequirements()
-        }
-    }
-    @objc var userPhone: String? {
-        didSet {
-            self.checkRequirements()
-        }
-    }
-    @objc var userEmail: String? {
-        didSet {
-            self.checkRequirements()
-        }
-    }
-    
-    @objc var techFirst: String? {
-        didSet {
-            self.checkRequirements()
-        }
-    }
-    @objc var techLast: String? {
-        didSet {
-            self.checkRequirements()
-        }
-    }
-    @objc var techPhone: String? {
-        didSet {
-            self.checkRequirements()
-        }
-    }
-    @objc var techEmail: String? {
-        didSet {
-            self.checkRequirements()
-        }
-    }
-    
-    @objc var loanPeriod: NSNumber? {
-        didSet {
-            self.checkRequirements()
-        }
-    }
-    
-    @objc var requiredCompleted: Bool = false
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
@@ -84,31 +32,7 @@ class LoanerConfigurationViewController: NSViewController {
      Checks to see if all the required items are filled and not empty and sets the `requiredCompleted` to true
     */
     private func checkRequirements() {
-        if (
-            self.userFirst?.isEmpty ?? true ||
-            self.userLast?.isEmpty ?? true ||
-            self.userPhone?.isEmpty ?? true ||
-            self.userEmail?.isEmpty ?? true ||
-            self.techFirst?.isEmpty ?? true ||
-            self.techLast?.isEmpty ?? true ||
-            self.techPhone?.isEmpty ?? true ||
-            self.techEmail?.isEmpty ?? true ||
-            self.loanPeriod == nil
-        )
-        {
-            DispatchQueue.main.async {
-                self.willChangeValue(forKey: "requiredCompleted")
-                self.requiredCompleted = false
-                self.didChangeValue(forKey: "requiredCompleted")
-            }
-        }
-        else {
-            DispatchQueue.main.async {
-                self.willChangeValue(forKey: "requiredCompleted")
-                self.requiredCompleted = true
-                self.didChangeValue(forKey: "requiredCompleted")
-            }
-        }
+        
     }
     
 }
